f7f6c5e02bf4dc4c247e186fbe39e020e47fdf60
ypy <fengchenziyi@sina.com>
2017-03-19 19:50:32 +0800

    rest
    3

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var express    = require('express');        
-var app        = express();                 
+var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
 
 
@@ -7,40 +7,84 @@ var mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/test");
 
 
-var User = require('./models');
+var Movie = require('./models');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;      
+var port = process.env.PORT || 8080;
 
 
-var router = express.Router();              
+var router = express.Router();
 
-router.use(function(req, res, next) {
+router.use(function (req, res, next) {
     console.log('Something is happening.');
-    next(); 
+    next();
 });
 
 
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+router.get('/', function (req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
 });
 
-router.route('/user')   
-    .post(function(req, res) {
-        
-        var user = new User();      
-        user.name = req.body.name; 
-        user.age=req.body.age;
-        user.save(function(err) {
+router.route('/movies')
+    .post(function (req, res) {
+        let movie = new Movie(req.body);
+        movie.save(function (err) {
             if (err)
                 res.send(err);
 
-            res.json({ message: 'user created!' });
+            res.json({ message: 'movie created!' });
         });
-        
+
+    })
+    .get(function (Req, res) {
+        Movie.find(function (err, movies) {
+            if (err) {
+                return res.send(err);
+            }
+            res.json(movies);
+        });
     });
+
+router.route('/movies/:id').put(function (req, res) {
+    Movie.findOne({ _id: req.params.id }, function (err, movie) {
+        if (err) {
+            return res.send(err);
+        }
+
+        for (prop in req.body) {
+            movie[prop] = req.body[prop];
+        }
+
+        // save the movie
+        movie.save(function (err) {
+            if (err) {
+                return res.send(err);
+            }
+
+            res.json({ message: 'Movie updated!' });
+        });
+    });
+}).get(function (req, res) {
+    Movie.findOne({ _id: req.params.id }, function (err, movie) {
+        if (err) {
+            return res.send(err);
+        }
+
+        res.json(movie);
+    });
+}).delete(function (req, res) {
+    Movie.remove({
+        _id: req.params.id
+    }, function (err, movie) {
+        if (err) {
+            return res.send(err);
+        }
+
+        res.json({ message: 'Successfully deleted' });
+    });
+});
 
 app.use('/api', router);
 
